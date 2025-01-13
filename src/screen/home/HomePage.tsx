80a47043012ebbab0d2d2b877e1efb3609d45395
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-01-13 23:50:22 +0900

    fix : Home 페이지 레이아웃 구현 적용 (아이콘추가필요)

--- src/screen/home/HomePage.tsx
+++ src/screen/home/HomePage.tsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from 'react';
+import { tm } from '../../utils/twMerge';
+
 const HomePage = () => {
-  return <div>메인페이지</div>;
+  const titleList: string[] = ['끊임없이 성장하는', '끊임없이 도전하는', '오늘과 내일이 다른'];
+  const [currentIdx, setCurrentIdx] = useState<number>(0);
+  const [title, setTitle] = useState<string>(titleList[currentIdx]);
+
+  const onClickChangeTitle = () => {
+    setCurrentIdx((prevIdx) => {
+      const nextIdx = (prevIdx + 1) % titleList.length; // 다음 인덱스 계산
+      setTitle(titleList[nextIdx]);
+      return nextIdx;
+    });
+  };
+  return (
+    <div className={tm('h-full flex flex-col justify-center items-center gap-2 select-none')}>
+      <h2 className={tm('text-3xl')}>안녕하세요!</h2>
+      <p className={tm('text-4xl font-bold text-pointColor cursor-pointer ')} onClick={onClickChangeTitle}>
+        {title}
+      </p>
+      <p className={tm('text-3xl')}>프론트엔드 개발자 이은솔입니다.</p>
+    </div>
+  );
 };
 
 export default HomePage;
