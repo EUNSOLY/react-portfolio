a5190aaf108c649ae967908d078ad3c1e7968a27
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-01-16 00:58:13 +0900

    add : about 페이지에 사용될 데이터 프레임 생성

---
+++ src/data/mock/mockupData.ts
@@ -0,0 +1,100 @@
+interface AboutDataType {
+  title: string;
+  icon: string;
+  content: string[];
+}
+export const skillsData: AboutDataType[] = [
+  {
+    title: 'HTML',
+    icon: '',
+    content: [
+      '웹 표준을 기반으로 시맨틱 마크업을 통해 웹 페이지 구조 작성 가능합니다.',
+      '적응형 / 반응형 웹사이트 제작이 가능하며 경험이 있습니다.',
+      '애니메이션과 트랜지션을 사용하여 레이아웃 효과 구현 경험이 있습니다.',
+      '외부 라이브러리 TailwindCSS , BootStrap을 사용하여 레이아웃 작성 및 스타일일링이 가능합니다.',
+    ],
+  },
+  {
+    title: 'CSS',
+    icon: '',
+    content: [],
+  },
+  {
+    title: 'Javascript',
+    icon: '',
+    content: [],
+  },
+  {
+    title: 'TypeScript',
+    icon: '',
+    content: [],
+  },
+  {
+    title: 'React',
+    icon: '',
+    content: [],
+  },
+  {
+    title: 'NextJs',
+    icon: '',
+    content: [],
+  },
+  {
+    title: 'MySql',
+    icon: '',
+    content: [],
+  },
+  {
+    title: 'MongoDB',
+    icon: '',
+    content: [],
+  },
+  {
+    title: 'Flutter',
+    icon: '',
+    content: [],
+  },
+  {
+    title: 'ReduxToolkit',
+    icon: '',
+    content: [],
+  },
+];
+
+export const toolsData: AboutDataType[] = [
+  { title: 'Notion', icon: '', content: [] },
+  { title: 'Slack', icon: '', content: [] },
+  { title: 'DBeaver', icon: '', content: [] },
+  { title: 'Photoshop', icon: '', content: [] },
+  { title: 'Lllustrator', icon: '', content: [] },
+  { title: 'Figma', icon: '', content: [] },
+  { title: 'Github', icon: '', content: [] },
+  { title: 'GithubDesktop', icon: '', content: [] },
+];
+
+export const educationData = [
+  {
+    title: '그린 컴퓨터아카데미',
+    content: '<산대특>리액트(React)\n리액트네이티브 EXPO 활용\n프론트엔드개발자 양성과정',
+    date: '2022.12.19 - 2023.06.27',
+  },
+  {
+    title: '대전과학기술대학교',
+    content: '외식조리제빵계열 - 조리전공',
+    date: '2013.03 - 2015.02',
+  },
+];
+export const certificateData = [
+  {
+    title: '웹디자인 기능사',
+    date: '2023.04',
+  },
+  {
+    title: '컴퓨터그래픽스 운용기능사',
+    date: '2022.07',
+  },
+  {
+    title: 'SQL개발자(SQLD)',
+    date: '2024.04',
+  },
+];
