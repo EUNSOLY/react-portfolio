5ece52d0e5c672c111a80a9e57694af4f146b171
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-01-16 00:57:18 +0900

    fix : 새로고침 시 경로 초기화가 안되는 이슈로 useEffect 경로 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import { MenuNameType } from '../utils/type';
 import { useAppDispatch, useAppSelector } from '../utils/store';
 import { setSelectMenuName } from '../data/menuSlice';
 import useNavigation from '../router/hooks/useNavigation';
+import { useEffect } from 'react';
 const Header = () => {
   const navList: { name: MenuNameType; path: string }[] = [
     { name: 'HOME', path: '/' },
@@ -20,6 +21,10 @@ const Header = () => {
     navi.naviToIntegration(path);
   };
 
+  // 새로고침 할 때 주소 새로고침 안되는 현상 있어 최초 1회 루트 경로 적용
+  useEffect(() => {
+    navi.naviToMain();
+  }, []);
   return (
     <div className={tm('flex flex-wrap items-center justify-between h-[50px] gap-2')}>
       <h1 className={tm('min-w-16 w-16')}>
