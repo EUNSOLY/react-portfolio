88d55e06c0d7cb1ff60d45e6c2ae897c3a7238f5
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-01-16 01:12:06 +0900

    fix : 어바웃페이지 레이아웃 수정 작업 중

--- src/screen/about/AboutPage.tsx
+++ src/screen/about/AboutPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import profile from '../../assets/image/profile.jpeg';
 import removeProfile from '../../assets/image/removeProfile.png';
 import profileCharacter from '../../assets/image/removeProfileCharacter.png';
-import { educationData } from '../../data/mock/mockupData';
+import { certificateData, educationData } from '../../data/mock/mockupData';
 import { tm } from '../../utils/twMerge';
 import useWindowDimensions from '../../utils/useWindowDimensions';
 
@@ -25,8 +25,13 @@ const AboutPage = () => {
     >
       <div className={tm('flex flex-col w-full h-full')}>
         {isMobile && (
-          <div className={tm('flex justify-center items-center border-4 rounded-full ')}>
-            <img src={removeProfile} alt="profile" className={tm('w-64 -mt-5', '')} />
+          <div
+            className={tm(
+              'self-center',
+              'flex justify-center items-center border-4 rounded-full overflow-hidden min-h-64 min-w-64 w-64'
+            )}
+          >
+            <img src={removeProfile} alt="profile" className={tm('min-w-64 w-64 -mt-1', '')} />
           </div>
         )}
 
@@ -49,17 +54,16 @@ const AboutPage = () => {
         </div>
 
         <div className={tm('')}>
-          <p className={tm('font-bold text-xl border-b border-dark pb-2')}>My Education</p>
+          <p className={tm('font-bold text-xl border-b border-dark pb-2')}>My Certificate</p>
           <div className={tm('flex gap-6')}>
-            {educationData.map((data, i) => {
+            {certificateData.map((data, i) => {
               return (
-                <div key={`${data.title}-${i}`} className={tm('flex flex-col gap-2 py-2 text-xs')}>
+                <div key={`${data.title}-${i}`} className={tm('flex flex-col py-2 text-xs')}>
                   <p className={tm('font-bold text-sm', 'flex items-center gap-1')}>
                     <span className={tm('text-transparent bg-dark w-[5px] h-[5px] rounded-full')} />
                     <span>{data.title}</span>
                   </p>
-                  <p className={'whitespace-pre-wrap'}>{data.content}</p>
-                  <p className="text-[10px] ">{data.date}</p>
+                  <p className="text-[10px] pl-2">{data.date}</p>
                 </div>
               );
             })}
@@ -76,8 +80,8 @@ const AboutPage = () => {
                     <span className={tm('text-transparent bg-dark w-[5px] h-[5px] rounded-full')} />
                     <span>{data.title}</span>
                   </p>
-                  <p className={'whitespace-pre-wrap'}>{data.content}</p>
-                  <p className="text-[10px] ">{data.date}</p>
+                  <p className={'whitespace-pre-wrap pl-2'}>{data.content}</p>
+                  <p className="text-[10px] pl-2">{data.date}</p>
                 </div>
               );
             })}
