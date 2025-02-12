b0e3d039629695c47f5d0a7a0a53ec0e8ce7e4ef
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-02-12 20:27:44 +0900

    모바일 기기 및 브라우저 모바일 사이즈 훅 생성

---
+++ src/utils/useIsMobile.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+export const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      // 사용자 브라우저 정보가 있다면 브라우저 정보 반환 없다면 빈 문자열 반환
+      const userAgent = typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
+      // 정규식 모바일 장치 키워드 포함 체크
+      const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+      setIsMobile(mobile || ('ontouchstart' in window && window.innerWidth <= 1024) || window.innerWidth <= 480);
+      console.log(window.navigator, 'window');
+      console.log(window, 'window');
+    };
+
+    checkIsMobile();
+    window.addEventListener('resize', checkIsMobile);
+    return () => window.removeEventListener('resize', checkIsMobile);
+  }, []);
+
+  return isMobile;
+};
