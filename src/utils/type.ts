e5eddd80c5a318caa568f5255e61fac0396309ab
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-02-12 20:28:11 +0900

    add : type 추가

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,1 +1,10 @@
 export type MenuNameType = 'HOME' | 'ABOUT' | 'PROJECTS' | 'CONNECT';
+
+export type ProjdectType = {
+  title: string;
+  description: string;
+  techs: string[];
+  mockupImage: string;
+  githubLink: string;
+  liveLink: string;
+};
